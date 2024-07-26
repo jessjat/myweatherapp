05c66b2732fa2801df62dec34b207bf1cc3a405e
jessjat <mutanujat@gmail.com>
2024-07-26 23:54:40 +0300

    Added the search functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function refreshWeather(response){
-    
+let temperatureElement=document.querySelector("#temperature");
+let temperature=response.data.temperature.current; 
+
+cityElement.innerHTML=response.data.city;
+temperatureElement.innerHTML=math.round(temperature);
+
+ 
 }
 
 function searchCity(city){
 let apiKey= "3ob9403f647925649063e2b120atb379";
-let apiUrl="https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metrc";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metrc`;
 axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -13,7 +19,7 @@ function handlesearchSubmit(event){
 event.preventDefault();
 let searchInput=document.querySelector("#search-form-input");
 let cityElement=document.querySelector("#city");
-cityElement=searchInput.value;
+
 searchCity(searchInput.value);
 
 
@@ -22,4 +28,5 @@ searchCity(searchInput.value);
 
 
 let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit".handlesearchSubmit);
+searchFormElement.addEventListener("submit",handlesearchSubmit);
+searchCity("Paris");
