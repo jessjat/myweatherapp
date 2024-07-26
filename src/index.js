cce062aabf968f162fbbfe2811dfb646c5e5f151
jessjat <mutanujat@gmail.com>
2024-07-27 00:41:47 +0300

    Rectified errors in index.css and index.html

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 function refreshWeather(response){
 let temperatureElement=document.querySelector("#temperature");
 let temperature=response.data.temperature.current; 
+let cityElement=document.querySelector("#city");
 
 cityElement.innerHTML=response.data.city;
-temperatureElement.innerHTML=math.round(temperature);
+temperatureElement.innerHTML=Math.round(temperature);
 
  
 }
 
 function searchCity(city){
 let apiKey= "3ob9403f647925649063e2b120atb379";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metrc`;
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(refreshWeather);
 }
 
 
-function handlesearchSubmit(event){
+function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput=document.querySelector("#search-form-input");
-let cityElement=document.querySelector("#city");
 
 searchCity(searchInput.value);
 
@@ -28,5 +28,5 @@ searchCity(searchInput.value);
 
 
 let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handlesearchSubmit);
+searchFormElement.addEventListener("submit",handleSearchSubmit);
 searchCity("Paris");
