56e0dce9c1ba7db738d8340bafa69826a30547b2
jessjat <mutanujat@gmail.com>
2024-07-26 23:26:11 +0300

    Added axios and API key and API Url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response){
+    
+}
+
+function searchCity(city){
+let apiKey= "3ob9403f647925649063e2b120atb379";
+let apiUrl="https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metrc";
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handlesearchSubmit(event){
-    event.preventDefault();
-    let searchInput=document.querySelector("#search-form-input");
+event.preventDefault();
+let searchInput=document.querySelector("#search-form-input");
 let cityElement=document.querySelector("#city");
 cityElement=searchInput.value;
+searchCity(searchInput.value);
+
 
 }
 
