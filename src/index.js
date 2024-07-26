0cd3125ff2e97f16ce15f10be542a2326c777ffc
jessjat <mutanujat@gmail.com>
2024-07-26 23:00:13 +0300

    Added basic js for search

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function handlesearchSubmit(event){
+    event.preventDefault();
+    let searchInput=document.querySelector("#search-form-input");
+let cityElement=document.querySelector("#city");
+cityElement=searchInput.value;
+
+}
+
+
+
+let searchFormElement=document.querySelector("#search-form");
+searchFormElement.addEventListener("submit".handlesearchSubmit);
